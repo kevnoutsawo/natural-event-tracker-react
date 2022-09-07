81bf2cc8b6ae34bb1473fc53376edc76351b4f83
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2022-09-07 23:35:25 +0100

    Updated NASA EONET API route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     const fetchEvents = async () => {
       setLoading(true)
       
-      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const res = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events')
       const { events } = await res.json()
       
       setEventData(events)
